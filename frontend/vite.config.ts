82cf8ee48af46a46ee40650f51af3bbae79f8a35
Ibrahim Mammadov <zxyctn@gmail.com>
2023-05-19 12:00:32 +0200

    Add icons and metadata for PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,ts,tsx,css,html,ico,png,svg}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: '_tracker',
+        short_name: '_tracker',
+        description: 'Fitness tracker application',
+        theme_color: '#00FFB2',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        scope: '/',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        orientation: 'portrait',
+      },
+    }),
+  ],
+});
