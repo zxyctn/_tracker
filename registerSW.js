bd55c2399e89b499f5f5a100878cc1519cd1eb79
zxyctn <zxyctn@gmail.com>
2023-05-26 09:54:33 +0000

    Deploying to gh-pages from @ zxyctn/_tracker@07a2c43d017f9c86354c91bef131cce245a0404e 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/_tracker/sw.js', { scope: '/_tracker/' })})}
