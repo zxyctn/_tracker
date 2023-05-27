071029b6b42c283ba13e254efafea01bf53928fb
Ibrahim Mammadov <zxyctn@gmail.com>
2023-05-27 19:08:29 +0200

    Add Groups

--- frontend/src/routes/Groups.tsx
+++ frontend/src/routes/Groups.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { getTextColors } from '../shared';
+import { Link } from 'react-router-dom';
 
 const Groups = () => {
-  return <div>Groups</div>;
+  const groups = useSelector((state: RootState) => state.groups);
+  const textColors = getTextColors(groups.length);
+
+  return (
+    <>
+      {groups.length > 0 ? (
+        groups.map((group, index) => (
+          <button className={`coloredBtn ${textColors[index]}`} key={index}>
+            <Link to={`${group.id}`}>{group.name}</Link>
+          </button>
+        ))
+      ) : (
+        <h1>...</h1>
+      )}
+    </>
+  );
 };
 
 export default Groups;
