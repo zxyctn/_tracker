002d0c1f7dcad00e6a066f8350be0165ac614423
Ibrahim Mammadov <zxyctn@gmail.com>
2023-05-28 09:06:17 +0200

    Add highlighting for today's weekday

--- frontend/src/routes/Weekdays.tsx
+++ frontend/src/routes/Weekdays.tsx
@@ -7,18 +7,25 @@ const Weekdays = () => {
   const weekdays: WeekdaysType = useLoaderData() as WeekdaysType;
   const textColors = getTextColors(Object.keys(weekdays).length);
 
+  const today = (new Date().getDay() + 6) % 7;
+
   return (
-    <>
+    <div className='grid gap-1'>
       {Object.keys(weekdays).length > 0 ? (
         Object.keys(weekdays).map((weekday, index) => (
-          <button className={`coloredBtn ${textColors[index]}`} key={index}>
+          <button
+            className={`${today === index ? 'today' : 'coloredBtn'} ${
+              textColors[index]
+            }`}
+            key={index}
+          >
             <Link to={`/d/${weekday.toLowerCase()}`}>{weekday}</Link>
           </button>
         ))
       ) : (
         <h1>...</h1>
       )}
-    </>
+    </div>
   );
 };
 
