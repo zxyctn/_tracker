a60f5ab5fb55f71251e160d5b59a12884bff9712
Ibrahim Mammadov <zxyctn@gmail.com>
2023-05-29 17:26:04 +0200

    Add edit handling for Groups

--- frontend/src/routes/Groups.tsx
+++ frontend/src/routes/Groups.tsx
@@ -2,10 +2,24 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 import { getTextColors } from '../shared';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setEdit } from '../slices/actionsSlice';
 
 const Groups = () => {
+  const edit = useSelector((state: RootState) => state.actions.edit);
   const groups = useSelector((state: RootState) => state.groups);
   const textColors = getTextColors(groups.length);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (edit.value && edit.result) {
+      dispatch(setEdit({ value: false, result: null }));
+    } else if (edit.value && edit.result === false) {
+      dispatch(setEdit({ value: false, result: null }));
+    }
+  }, [edit]);
 
   return (
     <>
