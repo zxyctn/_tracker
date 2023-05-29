ab1964f0e219bbdcbb32232005a27e2ee768b85b
Ibrahim Mammadov <zxyctn@gmail.com>
2023-05-29 11:42:50 +0200

    Add new Group route

--- frontend/src/routes/Group.tsx
+++ frontend/src/routes/Group.tsx
@@ -1,115 +1,59 @@
-import { useSelector } from 'react-redux';
-import { Link, useLoaderData } from 'react-router-dom';
-
-import { getTextColors } from '../shared';
-import type { RootState } from '../store';
-import type { RouteLoaderType } from '../types';
-import Button from '../components/Layout/Button';
-import { Droppable } from '@hello-pangea/dnd';
-import DraggableComponent from '../components/DND/DraggableComponent';
-import DeleteZone from '../components/DND/DeleteZone';
-import { useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { setAdd, setConfirm, setEdit } from '../slices/actionsSlice';
-import { removeExerciseGroup, setGroup } from '../slices/groupsSlice';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { RouteLoaderType } from '../types';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { useDispatch } from 'react-redux';
+import { setEdit } from '../slices/actionsSlice';
+import TextField from '../components/Input/TextField';
+import Input from '../components/Input/Input';
+import { setGroup } from '../slices/groupsSlice';
 
 const Group = () => {
   const { id } = useLoaderData() as RouteLoaderType;
+
   const edit = useSelector((state: RootState) => state.actions.edit);
-  const add = useSelector((state: RootState) => state.actions.add);
-  const confirm = useSelector((state: RootState) => state.actions.confirm);
   const group = useSelector(
     (state: RootState) => state.groups.find((g) => g.id === id)!
   );
-  const exercises = useSelector((state: RootState) =>
-    group.exercises.map((e) => state.exercises.find((ex) => ex.id === e)! || {})
-  );
-  const [initialGroup, setInitalGroup] = useState(group);
-  const textColors = getTextColors(exercises.length);
+
+  const [initialGroup] = useState(group);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const changeHandler = (newValue: string) => {
+    dispatch(
+      setGroup({
+        value: {
+          ...group,
+          name: newValue,
+        },
+      })
+    );
+  };
 
   useEffect(() => {
     if (edit.value && edit.result) {
       dispatch(setEdit({ value: false, result: null }));
-      setInitalGroup(group);
+      navigate(-1);
     } else if (edit.value && edit.result === false) {
       dispatch(setGroup({ value: initialGroup }));
       dispatch(setEdit({ value: false, result: null }));
+      navigate(-1);
     }
   }, [edit]);
 
-  useEffect(() => {
-    dispatch(
-      setAdd({
-        ...add,
-        prototype: null,
-        object: {
-          id: +new Date(),
-          name: '',
-          description: '',
-          sets: [],
-          history: [],
-        },
-        pages: 1,
-        page: 0,
-        id: group.id,
-        type: 'EXERCISE',
-      })
-    );
-
-    dispatch(
-      setConfirm({
-        ...confirm,
-        parent: id as number,
-        type: 'EXERCISE',
-      })
-    );
-  }, []);
-
   return (
-    <>
-      <Droppable droppableId={`group-${id}`} type='GROUP'>
-        {(provided) => (
-          <div
-            className='grid gap-1 overflow-x-auto scrollbar-hide'
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {exercises.length > 0 ? (
-              exercises.map((exercise, index) => (
-                <DraggableComponent
-                  id={`exercise-${exercise.id}`}
-                  index={index}
-                  key={`exercise-${exercise.id}`}
-                  isDragDisabled={!edit.value}
-                >
-                  <Button
-                    key={exercise.id}
-                    text={textColors[index]}
-                    edit={edit.value}
-                  >
-                    <Link
-                      to={
-                        edit.value ? `e/${exercise.id}` : `e/${exercise.id}/s`
-                      }
-                      className='text-center w-full'
-                    >
-                      {exercise.name}
-                    </Link>
-                  </Button>
-                </DraggableComponent>
-              ))
-            ) : (
-              <h1>...</h1>
-            )}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-
-      <DeleteZone droppableId='group-delete' type='GROUP' />
-    </>
+    <Input type={!edit.value} name='name'>
+      <TextField
+        type='text'
+        initial={group.name}
+        changeHandler={changeHandler}
+        disabled={!edit.value}
+        edit={edit.value}
+      />
+    </Input>
   );
 };
 
